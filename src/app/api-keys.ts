abdf5038acebaf12c5fd5ed39d65a2cfdd401d51
Guest User <Guest@epicodus-10.local>
2017-09-26 10:16:37 -0700

    Firebase DB setup with Add functionality.

---
+++ src/app/api-keys.ts
@@ -0,0 +1,7 @@
+export let masterFirebaseConfig = {
+    apiKey: 'AIzaSyDcewt2-W8Qa-jB_bUVBqVhViU98yr7fXg' ,
+    authDomain: 'firstdb-8beac.firebaseapp.com',
+    databaseURL: 'https://firstdb-8beac.firebaseio.com',
+    storageBucket: '',
+    messagingSenderId: '703827453572'
+  };
